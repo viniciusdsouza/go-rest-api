a6ecaa115f1d1d7ad310bd31f16c7bfd33d78145
viniciusdsouza <vduartesantiago@gmail.com>
2022-11-11 16:56:38 -0300

    feat: add POST method and .gitignore file

--- route.go
+++ route.go
@@ -6,9 +6,9 @@ import (
 )
 
 type Post struct {
-	Id    int		`json:"id"`
-	Title string	`json:"title"`
-	Text  string	`json:"text"`
+	Id    int    `json:"id"`
+	Title string `json:"title"`
+	Text  string `json:"text"`
 }
 
 var (
@@ -16,10 +16,10 @@ var (
 )
 
 func init() {
-	posts = []Post{Post{Id: 1, Title: "Title 1", Text: "Text 1"}}
+	posts = []Post{{Id: 1, Title: "Title 1", Text: "Text 1"}}
 }
 
-func getPosts(w http.ResponseWriter, r *http.Request) {
+func GetPosts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
 	result, err := json.Marshal(posts)
 	if err != nil {
@@ -28,5 +28,20 @@ func getPosts(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+}
+
+func AddPosts(w http.ResponseWriter, r *http.Request) {
+	var post Post
+	err := json.NewDecoder(r.Body).Decode(&post)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error": "Error unmarshalling the request"}`))
+		return
+	}
+	post.Id = len(posts) + 1
+	posts = append(posts, post)
+	w.WriteHeader(http.StatusCreated)
+	result, err := json.Marshal(post)
 	w.Write(result)
 }
--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
@@ -13,7 +14,8 @@ func main() {
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Up and running...")
 	})
-	router.HandleFunc("/posts", getPosts).Methods("GET")
+	router.HandleFunc("/posts", GetPosts).Methods("GET")
+	router.HandleFunc("/posts", AddPosts).Methods("POST")
 	log.Println("Server lintening on port", port)
 	log.Fatalln(http.ListenAndServe(port, router))
 }
