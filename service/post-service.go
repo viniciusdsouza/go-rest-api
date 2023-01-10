dbd231807c8565799129f38a9db9c0669b6b9040
viniciusdsouza <vduartesantiago@gmail.com>
2023-01-09 22:30:47 -0300

    feat: implements Posts Service layer

---
+++ service/post-service.go
@@ -0,0 +1,47 @@
+package service
+
+import (
+	"errors"
+	"math/rand"
+
+	"github.com/viniciusdsouza/go-rest-api/entity"
+	"github.com/viniciusdsouza/go-rest-api/repository"
+)
+
+var (
+	repo repository.PostRepository = repository.NewFirestoreRepository()
+)
+
+type PostService interface {
+	Validate(post *entity.Post) error
+	Create(post *entity.Post) (*entity.Post, error)
+	FindAll() ([]entity.Post, error)
+}
+
+type service struct {
+}
+
+func NewPostService() PostService {
+	return &service{}
+}
+
+func (*service) Validate(post *entity.Post) error {
+	if post == nil {
+		err := errors.New("the post is empty")
+		return err
+	}
+	if post.Title == "" {
+		err := errors.New("the post title is empty")
+		return err
+	}
+	return nil
+}
+
+func (*service) Create(post *entity.Post) (*entity.Post, error) {
+	post.ID = rand.Int63()
+	return repo.Save(post)
+}
+
+func (*service) FindAll() ([]entity.Post, error) {
+	return repo.FindAll()
+}
