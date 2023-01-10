9a99a3f330def6a58f857032f8e8cdf88f8f1efd
viniciusdsouza <vduartesantiago@gmail.com>
2023-01-10 20:47:06 -0300

    feat: adds FindOneById method in repository interface and implements it for firestore db

--- repository/firestore-repo.go
+++ repository/firestore-repo.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"log"
 
-	"github.com/viniciusdsouza/go-rest-api/entity"
 	"cloud.google.com/go/firestore"
+	"github.com/viniciusdsouza/go-rest-api/entity"
 	"google.golang.org/api/iterator"
 )
 
@@ -75,4 +75,43 @@ func (*repo) FindAll() ([]entity.Post, error) {
 		posts = append(posts, post)
 	}
 	return posts, nil
+}
+
+func (*repo) FindOneById(id int64) (*entity.Post, error) {
+
+	ctx := context.Background()
+	client, err := firestore.NewClient(ctx, projectId)
+	if err != nil {
+		log.Fatalf("Failed to Create a Firestore Client: %v", err)
+		return nil, err
+	}
+	defer client.Close()
+	var postEntity entity.Post
+
+	itr := client.Collection(collectionName).Documents(ctx)
+	for {
+		doc, err := itr.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to iterate the list of posts: %v", err)
+			return nil, err
+		}
+
+		post := entity.Post{
+			ID:    doc.Data()["ID"].(int64),
+			Title: doc.Data()["Title"].(string),
+			Text:  doc.Data()["Text"].(string),
+		}
+
+		if post.ID == id {
+			postEntity = entity.Post{
+				ID:    doc.Data()["ID"].(int64),
+				Title: doc.Data()["Title"].(string),
+				Text:  doc.Data()["Text"].(string),
+			}
+		}
+	}
+	return &postEntity, nil
 }
--- repository/posts-repo.go
+++ repository/posts-repo.go
@@ -5,4 +5,5 @@ import "github.com/viniciusdsouza/go-rest-api/entity"
 type PostRepository interface {
 	Save(post *entity.Post) (*entity.Post, error)
 	FindAll() ([]entity.Post, error)
+	FindOneById(id int64) (*entity.Post, error)
 }
