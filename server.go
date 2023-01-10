5f4b622198467a43a9714667253348ce2e654b39
viniciusdsouza <vduartesantiago@gmail.com>
2023-01-09 22:25:20 -0300

    refactor: update server implementing Clean Arch principles

--- server.go
+++ server.go
@@ -2,20 +2,24 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/viniciusdsouza/go-rest-api/controller"
+	router "github.com/viniciusdsouza/go-rest-api/http"
+)
+
+var (
+	postController controller.PostController = controller.NewPostController()
+	httpRouter     router.Router             = router.NewMuxRouter()
 )
 
 func main() {
-	router := mux.NewRouter()
 	const port string = ":8000"
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	httpRouter.GET("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Up and running...")
 	})
-	router.HandleFunc("/posts", GetPosts).Methods("GET")
-	router.HandleFunc("/posts", AddPosts).Methods("POST")
-	log.Println("Server lintening on port", port)
-	log.Fatalln(http.ListenAndServe(port, router))
+	httpRouter.GET("/posts", postController.GetPosts)
+	httpRouter.POST("/posts", postController.AddPost)
+
+	httpRouter.SERVE(port)
 }
